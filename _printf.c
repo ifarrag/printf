8739dfbf9b4ffe8d9b5e8c0a4cd26a91ed87d811
ifarrag <ifarrrag@gmail.com>
2024-05-28 04:40:00 -0700

    print with formats (s,c) and %%

---
+++ _printf.c
@@ -0,0 +1,72 @@
+#include <stdarg.h>
+#include <unisdt.h>
+int print_char(va_list ptr);
+int print_str(va_list ptr);
+int print_p(va_list ptr);
+/**
+ * _printf - print to stdout
+ * @format: format conversion specifiers
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int n, sum = 0, o;
+	va_list ptr;
+
+	int (*fptr[])(va_list ptr) = {print_char, print_str, print_p};
+	va_start(ptr, format);
+	if (format == NULL)
+		return (0);
+	for (n = 0; format[n] != '\0'; n++)
+	{
+		if (format[n] == '%')
+		{
+			n++;
+			for (o = 0; o < 3; o++)
+			{
+				if (format[n] == arr[o])
+				{
+					sum += (*fptr[o])(ptr);
+				}
+			}
+		}
+		else
+		{
+			sum += write(1, &format[n], 1);
+		}
+	}
+	va_end(ptr);
+	return (sum);
+}
+/**
+ * print_char - prints a char
+ * @ptr: va_arg pointer
+ */
+int print_char(va_list ptr)
+{
+	char c = (char)va_arg(ptr, int);
+
+	return (write(1, &c, 1));
+}
+/**
+ * print_p - prints % to stdout
+ * @ptr: va_arg pointer
+ */
+int print_p (va_list ptr)
+{
+	return (write(1, "%", 1));
+}
+/**
+ * print_str - print string to stdout
+ * @ptr: va_arg pointer
+ */
+int print_str(va_list ptr)
+{
+	unsigned int i = 0;
+	char *str = va_arg(ptr, char*);
+
+	while (str[i])
+	{
+		i++;
+	}
+	return (write(1, str, i);
+}
