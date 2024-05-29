5ff9597bca1a8a6acae9ad0019860854aa1fae45
ifarrag <ifarrrag@gmail.com>
2024-05-29 15:03:18 -0700

    specifiers s,c,%

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
 int print_char(va_list ptr);
 int print_str(va_list ptr);
@@ -6,6 +7,7 @@ int print_p(va_list ptr);
 /**
  * _printf - print to stdout
  * @format: format conversion specifiers
+ * Return: sum
  */
 int _printf(const char *format, ...)
 {
@@ -41,6 +43,7 @@ int _printf(const char *format, ...)
 /**
  * print_char - prints a char
  * @ptr: va_arg pointer
+ * Return: sum
  */
 int print_char(va_list ptr)
 {
@@ -51,20 +54,24 @@ int print_char(va_list ptr)
 /**
  * print_p - prints % to stdout
  * @ptr: va_arg pointer
+ * Return: sum
  */
-int print_p (va_list ptr __attribute__((unused)))
+int print_p(va_list ptr __attribute__((unused)))
 {
 	return (write(1, "%", 1));
 }
 /**
  * print_str - print string to stdout
  * @ptr: va_arg pointer
+ * Return: sum
  */
 int print_str(va_list ptr)
 {
 	unsigned int i = 0;
 	char *str = va_arg(ptr, char*);
 
+	if (str == 0)
+		return ((1, "(null)", 6));
 	while (str[i])
 	{
 		i++;
